943c61d8a4bbbb55f0f5436150410b29ec32d298
Boudeau <axel.boudeau@orionhealth.com>
2022-04-28 12:48:41 +0200

    adapt Paul calendar in a component

--- src/Components/Calendrier.js
+++ src/Components/Calendrier.js
@@ -1,11 +1,63 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-export default class Contact extends Component {
-    render() {
+const CalendrierPicker = () => {
+
+        const locales = {
+            "en-US": require("date-fns/locale/en-US"),
+        };
+        const localizer = dateFnsLocalizer({
+            format,
+            parse,
+            startOfWeek,
+            getDay,
+            locales,
+        });
+        
+        const events = [
+            {
+                title: "Big Meeting",
+                allDay: true,
+                start: new Date(2021, 6, 0),
+                end: new Date(2021, 6, 0),
+            },
+            {
+                title: "Vacation",
+                start: new Date(2021, 6, 7),
+                end: new Date(2021, 6, 10),
+            },
+            {
+                title: "Conference",
+                start: new Date(2021, 6, 20),
+                end: new Date(2021, 6, 23),
+            },
+        ];
+        const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+        const [allEvents, setAllEvents] = useState(events);
+    
+        function handleAddEvent() {
+            setAllEvents([...allEvents, newEvent]);
+        }
         return (
-            <div>
-                <h2>Calendrier</h2>
+            <div className="App">
+                <h2>Nouvelle évènement</h2>
+                <div>
+                    <input type="text" placeholder="Titre" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                    <DatePicker placeholderText="Début" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                    <DatePicker placeholderText="Fin" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                    <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                        Ajouter évènement
+                    </button>
+                </div>
+                <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
             </div>
-        )
-    }
-}
+            )
+        }
+export default CalendrierPicker;
--- src/Components/NavbarComp.js
+++ src/Components/NavbarComp.js
@@ -8,8 +8,8 @@ import {
 } from "react-router-dom";
 
 import Home from './Home';
-import Contact from './Calendrier';
-import About from './Check-list'
+import CalendrierPicker from './Calendrier';
+import Checklist from './Check-list'
 
 export default class NavbarComp extends Component {
     render() {
@@ -27,8 +27,8 @@ export default class NavbarComp extends Component {
                                 navbarScroll
                             >
                                 <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                                <Nav.Link as={Link} to="/about">Check-list</Nav.Link>
-                                <Nav.Link as={Link} to="/contact">Calendrier</Nav.Link>
+                                <Nav.Link as={Link} to="/Checklist">Check-list</Nav.Link>
+                                <Nav.Link as={Link} to="/Calendrier">Calendrier</Nav.Link>
 
                             </Nav>
 
@@ -37,11 +37,11 @@ export default class NavbarComp extends Component {
                 </div>
                 <div>
                     <Switch>
-                        <Route path="/about">
-                            <About />
+                        <Route path="/Checklist">
+                            <Checklist />
                         </Route>
-                        <Route path="/contact">
-                            <Contact />
+                        <Route path="/Calendrier">
+                            <CalendrierPicker />
                         </Route>
                         <Route path="/">
                             <Home />
